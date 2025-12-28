a5c48fe8065424cffa4ddf863ecdac6c022713f3
omadzelevan <levanomadze13@gmail.com>
2025-12-28 21:58:38 +0400

    initial

---
+++ src/header/header.jsx
@@ -0,0 +1,20 @@
+import "./header.css";
+export default function Header() {
+  return (
+    <header>
+      <div className="ads">
+        <div>
+          <img src="/assets/imgs/Vector.png" alt="Vector" />
+          <p className="p-bold">
+            Unlock the Magic of Travel with Travila - Your Gateway to
+            Extraordinary Experiences
+          </p>
+        </div>
+        <div>
+          <a href="">Get This Now </a>
+          <img src="/assets/imgs/Vector(1).png" alt="" />
+        </div>
+      </div>
+    </header>
+  );
+}
